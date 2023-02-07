c37981b133e51283bead9c305037963a83abe65e
Taillon Miguel <taillonmiguel@live.com>
2023-02-06 23:53:08 -0400

    implementado configurações na aplicationDbContext, criado ProductConfiguration e categoryConfiguration para que ao criar as tabelas seguir regras da configuração

--- CleanArchMvc/CleanArchMvc.Data/Context/ApplicationDbContext.cs
+++ CleanArchMvc/CleanArchMvc.Data/Context/ApplicationDbContext.cs
@@ -8,7 +8,7 @@ using System.Threading.Tasks;
 
 namespace CleanArchMvc.Data.Context
 {
-    internal class ApplicationDbContext : DbContext
+    public class ApplicationDbContext : DbContext
     {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) 
             : base(options)
