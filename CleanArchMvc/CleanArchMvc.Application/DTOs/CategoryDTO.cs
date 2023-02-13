91927f7ad23d1deedef723c93f43eeef74608956
Taillon Miguel <taillonmiguel@live.com>
2023-02-13 11:20:45 -0400

    criado Dto de product e category e feito o mapeamento com o automapper

---
+++ CleanArchMvc/CleanArchMvc.Application/DTOs/CategoryDTO.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanArchMvc.Application.DTOs
+{
+    public class CategoryDto
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "The Name is Required")]
+        [MinLength(3)]
+        [MaxLength(100)]
+        [DisplayName("Name")]
+        public string Name { get; set; }
+    }
+}
