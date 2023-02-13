aa180396fe5e629d4dc33c0e92f890a03b729829
Taillon Miguel <taillonmiguel@live.com>
2023-02-13 17:09:46 -0400

    implementação do service de Product e interface, utilizando o mapper

---
+++ CleanArchMvc/CleanArchMvc.Application/Services/ProductService.cs
@@ -0,0 +1,62 @@
+﻿using AutoMapper;
+using CleanArchMvc.Application.DTOs;
+using CleanArchMvc.Application.Interfaces;
+using CleanArchMvc.Domain.Entities;
+using CleanArchMvc.Domain.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanArchMvc.Application.Services
+{
+    public class ProductService : IProductService
+    {
+        public IProductRepository _productRepository;
+        public readonly IMapper _mapper;
+
+        public ProductService(IProductRepository productRepository, IMapper mapper)
+        {
+            _productRepository = productRepository;
+            _mapper = mapper;
+        }
+
+        public async Task<IEnumerable<ProductDTO>> GetProducts()
+        {
+            var product = await _productRepository.GetProduct();
+
+            return _mapper.Map<IEnumerable<ProductDTO>>(product);
+        }
+
+        public async Task<ProductDTO> GetById(int? id)
+        {
+           var product = await _productRepository.GetById(id);
+            return _mapper.Map<ProductDTO>(product);
+        }
+
+        public async Task<ProductDTO> GetProductCategory(int? id)
+        {
+            var product = await _productRepository.GetProductCategory(id);
+            return _mapper.Map<ProductDTO>(product);
+        }
+
+        public async Task Add(ProductDTO productDTO)
+        {
+            var product = _mapper.Map<Product>(productDTO);
+             await _productRepository.Create(product);
+        }
+
+        public async Task Update(ProductDTO productDTO)
+        {
+            var product = _mapper.Map<Product>(productDTO);
+            await _productRepository.Update(product);
+        }
+
+        public async Task Remove(int? id)
+        {
+            var product = await _productRepository.GetById(id);
+            await _productRepository.Remove(product);
+        }
+    }
+}
